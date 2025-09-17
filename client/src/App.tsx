1dfc3f6ad9f28089fa7e2d9a55db0fd08fa61d86
guppy <82202242+guppyadrian@users.noreply.github.com>
2025-09-16 22:14:55 -0700

    reimplement original frontend functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+function AppOld() {
   const [count, setCount] = useState(0)
 
   return (
@@ -28,6 +28,47 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+    </>
+  )
+}
+
+function App() {
+  
+  const wsRef = useRef<WebSocket>(null); //useRef so that it doesn't get reloaded on rerender
+  const [name, setName] = useState<string>('');
+  const [log, setLog] = useState<string[]>([]);
+
+  useEffect(() => {
+    wsRef.current = new WebSocket("ws://localhost:8080/ws");
+    wsRef.current.onopen = () => addToLog("Connected to Websocket");
+    wsRef.current.onmessage = event => {
+      addToLog(event.data);
+    }
+    wsRef.current.onclose = () => console.log("Websocket closed");
+
+    return () => wsRef.current?.close();
+  }, []);
+
+  const addToLog = (msg: string) => {
+    setLog(prev => [...prev, msg]);
+  }
+
+  const handleJoin = () => {
+    if (!name) return;
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      wsRef.current.send(JSON.stringify({type: 'join', room: 'ABCD', name}));
+    }
+  }
+
+  return (
+    <>
+      <h3>Kahoot clone - demo client</h3>
+      <input id='name' placeholder='Name' value={name} onChange={e => setName(e.target.value)}></input><button id='join' onClick={handleJoin}>Join</button>
+      <div id='log' style={{ marginTop: '1rem' }}>
+        {log.map((msg, i) => (
+          <div key={i}>{msg}</div>
+        ))}
+      </div>
     </>
   )
 }
