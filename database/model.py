95817cb2ab069c8fb62eb8f88611ef3465264f7e
Khoa Ta <khoata010106@gmail.com>
2025-10-03 14:59:05 -0700

    - define postgres in docker compose - list python library requirements - create simple sql model table

---
+++ database/model.py
@@ -0,0 +1,50 @@
+from sqlmodel import Field, create_engine, SQLModel, Session, column, JSON
+from typing import Optional, List
+from dotenv import load_dotenv
+import os
+
+class UserBase(SQLModel):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    username: str
+    email: str
+    admin: bool = False
+
+class User(UserBase, table=True):
+    hashed_password: str = Field()
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(UserBase):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+    admin: Optional[bool] = None
+
+class QuestionBase(SQLModel):
+    question_text: str
+    choices: List[str] = Field(sa_column=column(JSON))
+    correct_answer: str
+
+class Question(QuestionBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+class QuestionCreate(QuestionBase):
+    pass
+
+class QuestionUpdate(QuestionBase):
+    question_text: Optional[str] = None
+    choices: Optional[List[str]] = None
+    correct_answer: Optional[str] = None
+
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+engine = create_engine(DATABASE_URL, echo=True)
+SQLModel.metadata.create_all(engine)
+
+# Dependency
+def get_session():
+    with Session(engine) as session:
+        yield session
