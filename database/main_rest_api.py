4ea0e7e6982d79961bb2a63ed9bf7424708e16c1
jake-kolk <jkchutes@gmail.com>
2025-10-15 19:23:05 -0700

    made more readable

--- database/main_rest_api.py
+++ database/main_rest_api.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from database.question_router import router as question_router
 
 import auth.auth_router as auth_router
 import auth.user_router as user_router
@@ -21,6 +22,7 @@ app.add_middleware(
 
 app.include_router(auth_router.router, prefix="/auth")
 app.include_router(user_router.router)
+app.include_router(question_router)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ server/config.py
@@ -0,0 +1,9 @@
+import logging
+
+
+logging.basicConfig(
+    level=logging.DEBUG,  # or INFO, WARNING, ERROR
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+
+
